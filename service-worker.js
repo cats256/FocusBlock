249986965294a308ae1e3647aa52e35e08dd5747
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-11 16:07:42 -0500

    Rewrite service worker to initialize array when it is undefined

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-chrome.storage.local.set({ array: [] });
+chrome.storage.local.get(["array"]).then((result) => {
+    if (!result.array) {
+        chrome.storage.local.set({ array: [] });
+    }
+});
