31dc0185eb88b5a485116f6e9e9c15a5f0d36665
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-10 22:56:00 -0500

    Add addButton click event listener to add current tab URL to array

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,24 @@
-const array = ["www.youtube.com", "www.facebook.com", "www.instagram.com"];
-chrome.storage.local.set({ array })
+// const array = ["www.youtube.com", "www.facebook.com", "www.instagram.com"];
+// chrome.storage.local.set({ array })
+// chrome.storage.clear();
 
 chrome.storage.local.get(["array"]).then((result) => {
     if (!result.array) {
         chrome.storage.local.set({ array: [] });
     }
+});
+
+const addButton = document.getElementById("add-button");
+const removeButton = document.getElementById("remove-button");
+
+addButton.addEventListener("click", () => {
+    chrome.storage.local.get(["array"]).then((result) => {
+        const array = result.array;
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }).then((tab) => {
+            array.push(tab[0].url);
+            chrome.storage.local.set({ array }).then(() => {
+                console.log(array)
+            });
+        });
+    });
 });
