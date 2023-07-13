e37aabb4cc9d4f747bbf93055c776fcc992c2492
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-13 18:14:14 -0500

    Update font to self-hosted

--- popup/popup.js
+++ popup/popup.js
@@ -1,32 +1,37 @@
 const addBlockedSite = (array, baseUrl) => {
-    if (!array.includes(baseUrl)) {
-        array.push(baseUrl);
-        chrome.storage.local.set({ array });
-    }
+  if (!array.includes(baseUrl)) {
+    array.push(baseUrl);
+    chrome.storage.local.set({ array });
+  }
 };
 
 const removeBlockedSite = (array, baseUrl) => {
-    array = array.filter((element) => element !== baseUrl);
-    chrome.storage.local.set({ array });
+  array = array.filter((element) => element !== baseUrl);
+  chrome.storage.local.set({ array });
 };
 
 chrome.storage.local.get(["array"]).then(async (result) => {
-    const array = result.array;
-    const body = document.querySelector("body");
+  const array = result.array;
+  const body = document.querySelector("body");
 
-    array.forEach((element) => {
-        const div = document.createElement("div");
-        div.textContent = element;
-        body.appendChild(div);
-    });
+  array.forEach((element) => {
+    const div = document.createElement("div");
+    div.textContent = element;
+    body.appendChild(div);
+  });
 
-    const tabs = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-    const url = new URL(tabs[0].url);
-    const baseUrl = url.protocol + "//" + url.host;
+  const tabs = await chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  const url = new URL(tabs[0].url);
+  const baseUrl = url.protocol + "//" + url.host;
 
-    const addButton = document.getElementById("add-button");
-    const removeButton = document.getElementById("remove-button");
+  const addButton = document.getElementById("add-button");
+  const removeButton = document.getElementById("remove-button");
 
-    addButton.addEventListener("click", () => addBlockedSite(array, baseUrl));
-    removeButton.addEventListener("click", () => removeBlockedSite(array, baseUrl));
+  addButton.addEventListener("click", () => addBlockedSite(array, baseUrl));
+  removeButton.addEventListener("click", () =>
+    removeBlockedSite(array, baseUrl)
+  );
 });
