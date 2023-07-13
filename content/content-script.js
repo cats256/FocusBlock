be95d9a120978896acc77d026e4ba8b9ef32a38c
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-13 12:01:46 -0500

    Update code to correctly center elements and source lotus.svg

--- content/content-script.js
+++ content/content-script.js
@@ -10,37 +10,55 @@ chrome.storage.local.get(["array"]).then((result) => {
         :root {
           all: initial !important;
         }
-        
-        body {
-          font-family: "Inter", "Helvetica Neue", Helvetica, Arial, sans-serif !important;
-          display: flex !important;
-          flex-direction: column !important;
-          justify-content: center !important;
-          align-items: center !important;
-          height: 100vh !important;
-        }
       </style>
       <div id="focus-block"></div>
     `;
 
-    const focusBlock = body.getElementById("focus-block");
+    const focusBlock = body.querySelector("#focus-block");
     focusBlock.attachShadow({ mode: "open" });
 
     const shadowRoot = focusBlock.shadowRoot;
     shadowRoot.innerHTML = `
-      <p style="font-size: 32px">This site has been blocked by FocusBlock.</p>
-      <img src="icons/lotus-icon.svg" width="200px" height="200px" />
-      <br />
-      <p id="quote">Life begins at the end of your comfort zone.</p>
-      <div>
-        <button id="set-timeout">Set Timeout</button>
-        <button id="setting">Settings</button>
+      <div id="panel">
+        <p style="font-size: 28px">This site has been blocked by FocusBlock.</p>
+        <img width="200px" height="200px" />
+        <br />
+        <p id="quote" style="font-size: 20px">Life begins at the end of your comfort zone.</p>
+        <div>
+          <button id="set-timeout">Set Timeout</button>
+          <button id="setting">Settings</button>
+        </div>
       </div>
       <style>
-        button {
+      :host {
+        font-family: "Inter", "Helvetica Neue", Helvetica, Arial, sans-serif !important;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: rgb(184, 215, 248);
+      }
+
+      #panel {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 440px;
+        width: 720px;
+        background-color: white;
+        text-align: center;
+        border-radius: 16px;
+        box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
+      }      
+ 
+      button {
           margin: 10px;
         }
       </style>
     `;
+
+    const icon = shadowRoot.querySelector("img");
+    icon.src = chrome.runtime.getURL("../icons/lotus.svg");
   }
 });
