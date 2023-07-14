3abf45bc8ff3a094223db4239a26906a172d44db
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-13 19:22:20 -0500

    Refactor popup to use async/await instead of .then

--- popup/popup.js
+++ popup/popup.js
@@ -10,28 +10,24 @@ const removeBlockedSite = (array, baseUrl) => {
   chrome.storage.local.set({ array });
 };
 
-chrome.storage.local.get(["array"]).then(async (result) => {
-  const array = result.array;
-  const body = document.querySelector("body");
+const body = document.querySelector("body");
 
-  array.forEach((element) => {
-    const div = document.createElement("div");
-    div.textContent = element;
-    body.appendChild(div);
-  });
-
-  const tabs = await chrome.tabs.query({
-    active: true,
-    lastFocusedWindow: true,
-  });
-  const url = new URL(tabs[0].url);
-  const baseUrl = url.protocol + "//" + url.host;
-
-  const addButton = document.getElementById("add-button");
-  const removeButton = document.getElementById("remove-button");
-
-  addButton.addEventListener("click", () => addBlockedSite(array, baseUrl));
-  removeButton.addEventListener("click", () =>
-    removeBlockedSite(array, baseUrl)
-  );
+const array = (await chrome.storage.local.get(["array"])).array;
+array.forEach((element) => {
+  const div = document.createElement("div");
+  div.textContent = element;
+  body.appendChild(div);
 });
+
+const tabs = await chrome.tabs.query({
+  active: true,
+  lastFocusedWindow: true,
+});
+const url = new URL(tabs[0].url);
+const baseUrl = url.protocol + "//" + url.host;
+
+const addButton = document.getElementById("add-button");
+const removeButton = document.getElementById("remove-button");
+
+addButton.addEventListener("click", () => addBlockedSite(array, baseUrl));
+removeButton.addEventListener("click", () => removeBlockedSite(array, baseUrl));
