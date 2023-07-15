a94bcaf40b277e994be3de88f91a49bffcaa7d0b
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-15 13:10:33 -0500

    Add code to track time spent on websites

--- service-worker.js
+++ service-worker.js
@@ -2,4 +2,47 @@ chrome.storage.local.get().then((storage) => {
   if (!storage.blockedSites) {
     chrome.storage.local.set({ blockedSites: [] });
   }
+
+  if (!storage.tabsTime) {
+    chrome.storage.local.set({ tabsTime: {} });
+  }
+
+  if (!storage.activeTab) {
+    chrome.storage.local.set({
+      activeTab: {
+        url: null,
+        startTime: null,
+      },
+    });
+  }
 });
+
+chrome.tabs.onActivated.addListener(async () => {
+  const currentTime = new Date().getTime();
+  const storage = await chrome.storage.local.get();
+  const lastActiveTab = storage.activeTab.url;
+
+  if (lastActiveTab) {
+    if (storage.tabsTime[lastActiveTab]) {
+      storage.tabsTime[lastActiveTab] +=
+        currentTime - storage.activeTab.startTime;
+    } else {
+      storage.tabsTime[lastActiveTab] =
+        currentTime - storage.activeTab.startTime;
+    }
+    chrome.storage.local.set({ tabsTime: storage.tabsTime });
+  }
+
+  const tabs = await chrome.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
+  let url = new URL(tabs[0].url);
+  url = url.host.replace("www.", "");
+
+  chrome.storage.local.set({
+    activeTab: { url, startTime: currentTime },
+  });
+});
+
+// chrome.tabs.onRemoved.addListener(async () => {});
