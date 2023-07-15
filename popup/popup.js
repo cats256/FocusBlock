4de4bd9970a0d9b51111f7b4eb9713b44b767487
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-14 21:54:48 -0500

    Refactor code to toggle siteToggle properly

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,6 @@
 const addBlockedSite = (blockedSites, baseUrl) => {
-  if (!blockedSites.includes(baseUrl)) {
-    blockedSites.push(baseUrl);
-    chrome.storage.local.set({ blockedSites });
-  }
+  blockedSites.push(baseUrl);
+  chrome.storage.local.set({ blockedSites });
 };
 
 const removeBlockedSite = (blockedSites, baseUrl) => {
@@ -13,22 +11,15 @@ const removeBlockedSite = (blockedSites, baseUrl) => {
 const storage = await chrome.storage.local.get();
 const body = document.querySelector("body");
 
-const blockedSites = storage.blockedSites;
+let blockedSites = storage.blockedSites;
+let focusMode = storage.focusModeToggle;
 blockedSites.forEach((element) => {
   const div = document.createElement("div");
   div.textContent = element;
   body.appendChild(div);
 });
 
-const tabs = await chrome.tabs.query({
-  active: true,
-  lastFocusedWindow: true,
-});
-const url = new URL(tabs[0].url);
-const baseUrl = url.protocol + "//" + url.host;
-
 const focusToggle = document.getElementById("focus-toggle");
-let focusMode = storage.focusModeToggle;
 
 if (focusMode) {
   focusToggle.textContent = "Disable Focus Mode";
@@ -46,15 +37,27 @@ focusToggle.addEventListener("click", () => {
   chrome.storage.local.set({ focusModeToggle: focusMode });
 });
 
+const tabs = await chrome.tabs.query({
+  active: true,
+  lastFocusedWindow: true,
+});
+const url = new URL(tabs[0].url);
+const baseUrl = url.protocol + "//" + url.host;
 const siteToggle = document.getElementById("site-toggle");
+
 if (blockedSites.includes(baseUrl)) {
   siteToggle.textContent = "Remove From Blocklist";
-  siteToggle.addEventListener("click", () =>
-    removeBlockedSite(blockedSites, baseUrl)
-  );
 } else {
   siteToggle.textContent = "Add To Blocklist";
-  siteToggle.addEventListener("click", () =>
-    addBlockedSite(blockedSites, baseUrl)
-  );
 }
+
+siteToggle.addEventListener("click", () => {
+  if (blockedSites.includes(baseUrl)) {
+    siteToggle.textContent = "Add To Block List";
+    blockedSites = blockedSites.filter((element) => element !== baseUrl);
+  } else {
+    siteToggle.textContent = "Remove From Block List";
+    blockedSites.push(baseUrl);
+  }
+  chrome.storage.local.set({ blockedSites });
+});
