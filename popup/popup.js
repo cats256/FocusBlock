f2619263b1c4871193b060ed2449fa214feb1ef1
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-16 16:46:46 -0500

    Refactor code again

--- popup/popup.js
+++ popup/popup.js
@@ -12,33 +12,46 @@ chrome.runtime.onMessage.addListener(async () => {
 
   let { focusMode, blockedSites } = storage;
 
-  focusToggle.textContent = focusMode ? 'Disable Focus Mode' : 'Enable Focus Mode';
-  focusToggle.addEventListener('click', () => {
-    focusMode = !focusMode;
-    chrome.storage.local.set({ focusMode });
+  const setupFocusToggle = () => {
     focusToggle.textContent = focusMode ? 'Disable Focus Mode' : 'Enable Focus Mode';
-  });
+    focusToggle.addEventListener('click', () => {
+      focusMode = !focusMode;
+      chrome.storage.local.set({ focusMode });
+      focusToggle.textContent = focusMode ? 'Disable Focus Mode' : 'Enable Focus Mode';
+    });
+  };
 
-  siteToggle.textContent = blockedSites.includes(baseUrl) ? 'Remove From Blocklist' : 'Add To Blocklist';
-  siteToggle.addEventListener('click', () => {
-    if (blockedSites.includes(baseUrl)) {
-      siteToggle.textContent = 'Add To Block List';
-      blockedSites = blockedSites.filter((element) => element !== baseUrl);
-    } else {
-      siteToggle.textContent = 'Remove From Block List';
-      blockedSites.push(baseUrl);
-    }
-    chrome.storage.local.set({ blockedSites });
-  });
+  const setupSiteToggle = () => {
+    siteToggle.textContent = blockedSites.includes(baseUrl) ? 'Remove From Blocklist' : 'Add To Blocklist';
+    siteToggle.addEventListener('click', () => {
+      if (blockedSites.includes(baseUrl)) {
+        blockedSites = blockedSites.filter((element) => element !== baseUrl);
+        siteToggle.textContent = 'Add To Block List';
+      } else {
+        blockedSites.push(baseUrl);
+        siteToggle.textContent = 'Remove From Block List';
+      }
+      chrome.storage.local.set({ blockedSites });
+    });
+  };
 
-  const siteHrs = Math.floor((storage.tabsTime[domain] ?? 0) / 3600000);
-  const siteMins = Math.floor(((storage.tabsTime[domain] ?? 0) % 3600000) / 60000);
-  siteTodayUsage.textContent = `This Site: ${siteHrs} ${siteHrs === 0 ? 'hr' : 'hrs'} ${siteMins} ${siteMins === 0 ? 'min' : 'mins'}`;
+  const setupSiteTodayUsage = () => {
+    const siteHrs = Math.floor((storage.tabsTime[domain] ?? 0) / 3600000);
+    const siteMins = Math.floor(((storage.tabsTime[domain] ?? 0) % 3600000) / 60000);
+    siteTodayUsage.textContent = `This Site: ${siteHrs} ${siteHrs === 0 ? 'hr' : 'hrs'} ${siteMins} ${siteMins === 0 ? 'min' : 'mins'}`;
+  };
 
-  const sitesTodaySeconds = Object.values(storage.tabsTime).reduce((acc, curr) => acc + curr, 0);
-  const sitesHrs = Math.floor(sitesTodaySeconds / 3600000);
-  const sitesMins = Math.floor((sitesTodaySeconds % 3600000) / 60000);
-  sitesTodayUsage.textContent = `All Sites: ${sitesHrs} ${sitesHrs === 0 ? 'hr' : 'hrs'} ${sitesMins} ${sitesMins === 0 ? 'min' : 'mins'}`;
+  const setupSitesTodayUsage = () => {
+    const sitesTodaySeconds = Object.values(storage.tabsTime).reduce((acc, curr) => acc + curr, 0);
+    const sitesHrs = Math.floor(sitesTodaySeconds / 3600000);
+    const sitesMins = Math.floor((sitesTodaySeconds % 3600000) / 60000);
+    sitesTodayUsage.textContent = `All Sites: ${sitesHrs} ${sitesHrs === 0 ? 'hr' : 'hrs'} ${sitesMins} ${sitesMins === 0 ? 'min' : 'mins'}`;
+  };
+
+  setupFocusToggle();
+  setupSiteToggle();
+  setupSiteTodayUsage();
+  setupSitesTodayUsage();
 
   console.log(storage);
 });
