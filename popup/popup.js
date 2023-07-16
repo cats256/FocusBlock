14dc1fffb57472619e5b47adb6e624c2dcb45aad
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-15 19:25:40 -0500

    Add code to properly display time spent on websites

--- popup/popup.js
+++ popup/popup.js
@@ -1,71 +1,60 @@
-const addBlockedSite = (blockedSites, baseUrl) => {
-  blockedSites.push(baseUrl);
-  chrome.storage.local.set({ blockedSites });
-};
-
-const removeBlockedSite = (blockedSites, baseUrl) => {
-  blockedSites = blockedSites.filter((element) => element !== baseUrl);
-  chrome.storage.local.set({ blockedSites });
-};
-
 const storage = await chrome.storage.local.get();
-const body = document.querySelector("body");
-const focusToggle = document.getElementById("focus-toggle");
-let blockedSites = storage.blockedSites;
+const focusToggle = document.getElementById('focus-toggle');
+let { blockedSites } = storage;
 let focusMode = storage.focusModeToggle;
 
 if (focusMode) {
-  focusToggle.textContent = "Disable Focus Mode";
+  focusToggle.textContent = 'Disable Focus Mode';
 } else {
-  focusToggle.textContent = "Enable Focus Mode";
+  focusToggle.textContent = 'Enable Focus Mode';
 }
 
-focusToggle.addEventListener("click", () => {
+focusToggle.addEventListener('click', () => {
   if (focusMode) {
-    focusToggle.textContent = "Enable Focus Mode";
+    focusToggle.textContent = 'Enable Focus Mode';
   } else {
-    focusToggle.textContent = "Disable Focus Mode";
+    focusToggle.textContent = 'Disable Focus Mode';
   }
   focusMode = !focusMode;
   chrome.storage.local.set({ focusModeToggle: focusMode });
 });
 
-const tabs = await chrome.tabs.query({
-  active: true,
-  lastFocusedWindow: true,
-});
+const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
 const url = new URL(tabs[0].url);
-const baseUrl = url.protocol + "//" + url.host;
-const siteToggle = document.getElementById("site-toggle");
+const baseUrl = `${url.protocol}//${url.host}`;
+const siteToggle = document.getElementById('site-toggle');
 
 if (blockedSites.includes(baseUrl)) {
-  siteToggle.textContent = "Remove From Blocklist";
+  siteToggle.textContent = 'Remove From Blocklist';
 } else {
-  siteToggle.textContent = "Add To Blocklist";
+  siteToggle.textContent = 'Add To Blocklist';
 }
 
-siteToggle.addEventListener("click", () => {
+siteToggle.addEventListener('click', () => {
   if (blockedSites.includes(baseUrl)) {
-    siteToggle.textContent = "Add To Block List";
+    siteToggle.textContent = 'Add To Block List';
     blockedSites = blockedSites.filter((element) => element !== baseUrl);
   } else {
-    siteToggle.textContent = "Remove From Block List";
+    siteToggle.textContent = 'Remove From Block List';
     blockedSites.push(baseUrl);
   }
   chrome.storage.local.set({ blockedSites });
 });
 
-if (blockedSites.length !== 0) {
-  const blockListPanel = document.createElement("div");
-  const focusTab = document.getElementById("focus-tab");
+const siteTodayUsage = document.getElementById('site-today-usage');
+const tab = url.host.replace('www.', '');
+if (tab in storage.tabsTime) {
+  const hours = Math.floor(storage.tabsTime[tab] / 3600);
+  const minutes = Math.floor((storage.tabsTime[tab] % 3600) / 60);
 
-  focusTab.appendChild(blockListPanel);
-  blockListPanel.id = "block-list-panel";
-  blockedSites.forEach((element) => {
-    const div = document.createElement("div");
-    const url = new URL(element);
-
-    div.textContent = url.host.replace("www.", "");
-    blockListPanel.appendChild(div);
-  });
+  siteTodayUsage.textContent = `This Site: ${hours} ${hours === 0 ? 'hr' : 'hrs'} ${minutes} ${minutes === 0 ? 'min' : 'mins'}`;
 }
+
+const sitesTodayUsage = document.getElementById('sites-today-usage');
+const sitesTodaySeconds = Object.values(storage.tabsTime).reduce((acc, curr) => acc + curr, 0);
+const hours = Math.floor(sitesTodaySeconds / 3600);
+const minutes = Math.floor((sitesTodaySeconds % 3600) / 60);
+
+sitesTodayUsage.textContent = `All Sites: ${hours} ${hours === 0 ? 'hr' : 'hrs'} ${minutes} ${minutes === 0 ? 'min' : 'mins'}`;
+
+console.log(storage);
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,14 @@
+chrome.storage.local.set({
+  tabsTime: {
+    'chat.openai.com': 314000,
+    'developer.mozilla.org': 45000,
+    'discord.com': 538000,
+    'github.com': 175000,
+    'google.com': 24000,
+    'stackoverflow.com': 41000,
+  },
+});
+
 chrome.storage.local.get().then((storage) => {
   if (!storage.blockedSites) {
     chrome.storage.local.set({ blockedSites: [] });
@@ -11,17 +22,20 @@ chrome.storage.local.get().then((storage) => {
 const timer = async () => {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   if (tabs.length !== 0) {
-    let url = new URL(tabs[0].url);
-    url = url.host.replace('www.', '');
+    console.log('Tabs', tabs);
+    let url = tabs[0].url ? new URL(tabs[0].url) : new URL(tabs[0].pendingUrl);
+    if (url.host !== 'newtab') {
+      url = url.host.replace('www.', '');
 
-    const { tabsTime } = await chrome.storage.local.get();
-    if (tabsTime[url]) {
-      tabsTime[url] += 1000;
-    } else {
-      tabsTime[url] = 1000;
+      const { tabsTime } = await chrome.storage.local.get();
+      if (tabsTime[url]) {
+        tabsTime[url] += 5;
+      } else {
+        tabsTime[url] = 5;
+      }
+
+      chrome.storage.local.set({ tabsTime });
     }
-
-    chrome.storage.local.set({ tabsTime });
   }
 };
-setInterval(timer, 1000);
+setInterval(timer, 5000);
