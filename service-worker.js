855653389a4335d2843379332b08cfbbf81af9cb
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-16 16:47:21 -0500

    Simplify code

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,7 @@
-chrome.storage.local.clear();
-chrome.storage.local.set({
-  tabsTime: {
-    'chat.openai.com': 314000,
-    'developer.mozilla.org': 45000,
-    'discord.com': 538000,
-    'github.com': 175000,
-    'google.com': 24000,
-    'stackoverflow.com': 41000,
-  },
-});
-
 chrome.storage.local.get().then((storage) => {
   if (!storage.blockedSites) {
-    chrome.storage.local.set({ blockedSites: [] });
-  }
-
-  if (!storage.tabsTime) {
-    chrome.storage.local.set({ tabsTime: {} });
+    chrome.storage.local.set({ blockedSites: [], tabsTime: {} });
   }
 });
+
+chrome.runtime.onMessage.addListener(() => {});
