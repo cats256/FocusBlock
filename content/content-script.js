6d808ff143858793c681b89660d84144603f4f4c
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-18 13:06:49 -0500

    Fix time extension bug

--- content/content-script.js
+++ content/content-script.js
@@ -1,43 +1,61 @@
-let tabStartTime = Date.now();
-const url = new URL(window.location.origin);
-const domain = url.host.replace('www.', '');
+const blockSite = async () => {
+  const title = document.querySelector("title").cloneNode(true);
+  const contentHTML = await fetch(chrome.runtime.getURL("content/content.html"));
+  const contentText = await contentHTML.text();
+  const content = new DOMParser().parseFromString(contentText, "text/html");
+  const CSS = content.getElementById("CSS");
+  const icon = content.getElementById("icon");
+  const threeMinTimeout = content.getElementById("3-min-timeout");
+  const fiveMinTimeout = content.getElementById("5-min-timeout");
+  const fifteenMinTimeout = content.getElementById("15-min-timeout");
 
-window.addEventListener('focus', () => {
+  const setUnblockTime = (time) => {
+    chrome.storage.local.set({ unblockTimes: { [domain]: Date.now() + time } });
+    window.location.reload();
+  };
+
+  content.head.appendChild(title);
+  CSS.href = `${chrome.runtime.getURL("content/content.css")}`;
+  icon.src = chrome.runtime.getURL("icons/lotus.svg");
+  threeMinTimeout.addEventListener("click", () => setUnblockTime(3 * 60 * 1000));
+  fiveMinTimeout.addEventListener("click", () => setUnblockTime(5 * 60 * 1000));
+  fifteenMinTimeout.addEventListener("click", () => setUnblockTime(15 * 60 * 1000));
+  document.replaceChild(content.documentElement, document.documentElement);
+};
+
+const url = new URL(window.location.origin);
+const domain = url.host.replace("www.", "");
+let tabStartTime = Date.now();
+
+window.addEventListener("focus", () => {
   tabStartTime = Date.now();
 });
 
-window.addEventListener('blur', async () => {
+window.addEventListener("blur", async () => {
   const { tabsTime } = await chrome.storage.local.get();
-  tabsTime[domain] = (tabsTime[domain] ?? 0) + (Date.now() - tabStartTime);
-
-  chrome.storage.local.set({ tabsTime });
+  chrome.storage.local.set({
+    tabsTime: {
+      [domain]: (tabsTime[domain] ?? 0) + (Date.now() - tabStartTime),
+    },
+  });
   chrome.runtime.sendMessage(null);
 });
 
 chrome.storage.local.get().then(async (storage) => {
+  const siteInBlockList = storage.blockedSites.includes(window.location.origin);
   const pastUnblockTime = (storage.unblockTimes[domain] ?? 0) < Date.now();
 
-  if (storage.blockedSites.includes(window.location.origin) && pastUnblockTime) {
-    const title = document.querySelector('title').cloneNode(true);
-    const contentHTML = await fetch(chrome.runtime.getURL('content/content.html'));
-    const contentText = await contentHTML.text();
-    const content = new DOMParser().parseFromString(contentText, 'text/html');
-    const CSS = content.getElementById('CSS');
-    const icon = content.getElementById('icon');
-    const threeMinTimeout = content.getElementById('3-min-timeout');
-    const fiveMinTimeout = content.getElementById('5-min-timeout');
-    const fifteenMinTimeout = content.getElementById('15-min-timeout');
+  if (siteInBlockList && pastUnblockTime) {
+    blockSite();
+  } else if (siteInBlockList && storage.unblockTimes[domain]) {
+    const checkUnblock = setInterval(() => {
+      const thirtySecondsBeforeUnblock = storage.unblockTimes[domain] < Date.now() + 30 * 1000;
 
-    const setUnblockTime = (time) => {
-      chrome.storage.local.set({ unblockTimes: { [domain]: Date.now() + time } });
-    };
-
-    content.head.appendChild(title);
-    CSS.href = chrome.runtime.getURL('content/content.css');
-    icon.src = chrome.runtime.getURL('icons/lotus.svg');
-    threeMinTimeout.addEventListener('click', () => setUnblockTime(3 * 60 * 1000));
-    fiveMinTimeout.addEventListener('click', () => setUnblockTime(5 * 60 * 1000));
-    fifteenMinTimeout.addEventListener('click', () => setUnblockTime(15 * 60 * 1000));
-    document.replaceChild(content.documentElement, document.documentElement);
+      if (thirtySecondsBeforeUnblock) {
+        alert("You have 30 seconds left before this site is blocked.");
+        setTimeout(blockSite, 30 * 1000);
+        clearInterval(checkUnblock);
+      }
+    }, 10000);
   }
 });
--- popup/popup.js
+++ popup/popup.js
@@ -2,34 +2,36 @@ chrome.runtime.onMessage.addListener(async () => {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   const url = new URL(tabs[0].url);
   const baseUrl = `${url.protocol}//${url.host}`;
-  const domain = url.host.replace('www.', '');
+  const domain = url.host.replace("www.", "");
 
-  const focusToggle = document.getElementById('focus-toggle');
-  const siteToggle = document.getElementById('site-toggle');
-  const siteTodayUsage = document.getElementById('site-today-usage');
-  const sitesTodayUsage = document.getElementById('sites-today-usage');
+  const focusToggle = document.getElementById("focus-toggle");
+  const siteToggle = document.getElementById("site-toggle");
+  const siteTodayUsage = document.getElementById("site-today-usage");
+  const sitesTodayUsage = document.getElementById("sites-today-usage");
   const storage = await chrome.storage.local.get();
 
   let { focusMode, blockedSites } = storage;
 
   const setupFocusToggle = () => {
-    focusToggle.textContent = focusMode ? 'Disable Focus Mode' : 'Enable Focus Mode';
-    focusToggle.addEventListener('click', () => {
+    focusToggle.textContent = focusMode ? "Disable Focus Mode" : "Enable Focus Mode";
+    focusToggle.addEventListener("click", () => {
       focusMode = !focusMode;
       chrome.storage.local.set({ focusMode });
-      focusToggle.textContent = focusMode ? 'Disable Focus Mode' : 'Enable Focus Mode';
+      focusToggle.textContent = focusMode ? "Disable Focus Mode" : "Enable Focus Mode";
     });
   };
 
   const setupSiteToggle = () => {
-    siteToggle.textContent = blockedSites.includes(baseUrl) ? 'Remove From Blocklist' : 'Add To Blocklist';
-    siteToggle.addEventListener('click', () => {
+    siteToggle.textContent = blockedSites.includes(baseUrl)
+      ? "Remove From Blocklist"
+      : "Add To Blocklist";
+    siteToggle.addEventListener("click", () => {
       if (blockedSites.includes(baseUrl)) {
         blockedSites = blockedSites.filter((element) => element !== baseUrl);
-        siteToggle.textContent = 'Add To Block List';
+        siteToggle.textContent = "Add To Block List";
       } else {
         blockedSites.push(baseUrl);
-        siteToggle.textContent = 'Remove From Block List';
+        siteToggle.textContent = "Remove From Block List";
       }
       chrome.storage.local.set({ blockedSites });
     });
@@ -38,14 +40,18 @@ chrome.runtime.onMessage.addListener(async () => {
   const setupSiteTodayUsage = () => {
     const siteHrs = Math.floor((storage.tabsTime[domain] ?? 0) / 3600000);
     const siteMins = Math.floor(((storage.tabsTime[domain] ?? 0) % 3600000) / 60000);
-    siteTodayUsage.textContent = `This Site: ${siteHrs} ${siteHrs === 0 ? 'hr' : 'hrs'} ${siteMins} ${siteMins === 0 ? 'min' : 'mins'}`;
+    siteTodayUsage.textContent = `This Site: ${siteHrs} ${
+      siteHrs === 0 ? "hr" : "hrs"
+    } ${siteMins} ${siteMins === 0 ? "min" : "mins"}`;
   };
 
   const setupSitesTodayUsage = () => {
     const sitesTodaySeconds = Object.values(storage.tabsTime).reduce((acc, curr) => acc + curr, 0);
     const sitesHrs = Math.floor(sitesTodaySeconds / 3600000);
     const sitesMins = Math.floor((sitesTodaySeconds % 3600000) / 60000);
-    sitesTodayUsage.textContent = `All Sites: ${sitesHrs} ${sitesHrs === 0 ? 'hr' : 'hrs'} ${sitesMins} ${sitesMins === 0 ? 'min' : 'mins'}`;
+    sitesTodayUsage.textContent = `All Sites: ${sitesHrs} ${
+      sitesHrs === 0 ? "hr" : "hrs"
+    } ${sitesMins} ${sitesMins === 0 ? "min" : "mins"}`;
   };
 
   setupFocusToggle();
@@ -54,4 +60,6 @@ chrome.runtime.onMessage.addListener(async () => {
   setupSitesTodayUsage();
 });
 
-chrome.storage.local.get().then((storage) => { console.log(storage); });
+chrome.storage.local.get().then((storage) => {
+  console.log(storage);
+});
