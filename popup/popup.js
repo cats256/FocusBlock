c9520cd83955c347fd139a04ce15d6cdfb1ec74a
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-20 09:32:41 -0500

    Make some changes to the structure

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,9 @@
+const pomodoroHTML = await fetch("pomodoro/pomodoro.html");
+const pomodoroHTMLText = await pomodoroHTML.text();
+
+const pomodoroTimer = document.getElementById("pomodoro-timer");
+pomodoroTimer.innerHTML = pomodoroHTMLText;
+
 chrome.runtime.onMessage.addListener(async () => {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   const url = new URL(tabs[0].url);
@@ -22,9 +28,7 @@ chrome.runtime.onMessage.addListener(async () => {
   };
 
   const setupSiteToggle = () => {
-    siteToggle.textContent = blockedSites.includes(baseUrl)
-      ? "Remove From Blocklist"
-      : "Add To Blocklist";
+    siteToggle.textContent = blockedSites.includes(baseUrl) ? "Remove From Blocklist" : "Add To Blocklist";
     siteToggle.addEventListener("click", () => {
       if (blockedSites.includes(baseUrl)) {
         blockedSites = blockedSites.filter((element) => element !== baseUrl);
@@ -42,18 +46,18 @@ chrome.runtime.onMessage.addListener(async () => {
   const setupSiteTodayUsage = () => {
     const siteHrs = Math.floor((storage.tabsTime[domain] ?? 0) / 3600000);
     const siteMins = Math.floor(((storage.tabsTime[domain] ?? 0) % 3600000) / 60000);
-    siteTodayUsage.textContent = `This Site: ${siteHrs} ${
-      siteHrs === 0 ? "hr" : "hrs"
-    } ${siteMins} ${siteMins === 0 ? "min" : "mins"}`;
+    siteTodayUsage.textContent = `This Site: ${siteHrs} ${siteHrs === 0 ? "hr" : "hrs"} ${siteMins} ${
+      siteMins === 0 ? "min" : "mins"
+    }`;
   };
 
   const setupSitesTodayUsage = () => {
     const sitesTodaySeconds = Object.values(storage.tabsTime).reduce((acc, curr) => acc + curr, 0);
     const sitesHrs = Math.floor(sitesTodaySeconds / 3600000);
     const sitesMins = Math.floor((sitesTodaySeconds % 3600000) / 60000);
-    sitesTodayUsage.textContent = `All Sites: ${sitesHrs} ${
-      sitesHrs === 0 ? "hr" : "hrs"
-    } ${sitesMins} ${sitesMins === 0 ? "min" : "mins"}`;
+    sitesTodayUsage.textContent = `All Sites: ${sitesHrs} ${sitesHrs === 0 ? "hr" : "hrs"} ${sitesMins} ${
+      sitesMins === 0 ? "min" : "mins"
+    }`;
   };
 
   setupFocusToggle();
@@ -62,6 +66,7 @@ chrome.runtime.onMessage.addListener(async () => {
   setupSitesTodayUsage();
 });
 
+// For debugging
 chrome.storage.local.get().then((storage) => {
   console.log(storage);
 });
