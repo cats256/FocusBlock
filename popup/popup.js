536183a0162baefa014cd6f21f49676e484ca5cd
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-20 11:48:11 -0500

    Fix proper switching between panels

--- popup/popup.js
+++ popup/popup.js
@@ -1,88 +1,27 @@
+const pomodoroTimer = document.getElementById("pomodoro-timer");
 const pomodoroHTML = await fetch("pomodoro/pomodoro.html");
 const pomodoroHTMLText = await pomodoroHTML.text();
-
-const pomodoroTimer = document.getElementById("pomodoro-timer");
 pomodoroTimer.innerHTML = pomodoroHTMLText;
 
 const focusTab = document.getElementById("focus-tab");
+const focusBlock = document.getElementById("focus-block");
 
 const timer = document.getElementById("timer");
-const powerSettings = document.getElementById("power-settings");
-
 timer.addEventListener("click", () => {
   focusTab.style.display = "none";
   pomodoroTimer.style.display = "block";
-  timer.style.backgroundColor = rgb(205, 205, 205);
+  timer.style.backgroundColor = "rgb(205, 205, 205)";
   powerSettings.style.backgroundColor = "";
+  focusBlock.textContent = "Pomorodo Timer";
 });
 
+const powerSettings = document.getElementById("power-settings");
 powerSettings.addEventListener("click", () => {
   focusTab.style.display = "flex";
   pomodoroTimer.style.display = "none";
-  powerSettings.style.backgroundColor = rgb(205, 205, 205);
+  powerSettings.style.backgroundColor = "rgb(205, 205, 205)";
   timer.style.backgroundColor = "";
-});
-
-chrome.runtime.onMessage.addListener(async () => {
-  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-  const url = new URL(tabs[0].url);
-  const baseUrl = `${url.protocol}//${url.host}`;
-  const domain = url.host.replace("www.", "");
-
-  const focusToggle = document.getElementById("focus-toggle");
-  const siteToggle = document.getElementById("site-toggle");
-  const siteTodayUsage = document.getElementById("site-today-usage");
-  const sitesTodayUsage = document.getElementById("sites-today-usage");
-  const storage = await chrome.storage.local.get();
-
-  let { focusMode, blockedSites } = storage;
-
-  const setupFocusToggle = () => {
-    focusToggle.textContent = focusMode ? "Disable Focus Mode" : "Enable Focus Mode";
-    focusToggle.addEventListener("click", () => {
-      focusMode = !focusMode;
-      focusToggle.textContent = focusMode ? "Disable Focus Mode" : "Enable Focus Mode";
-      chrome.storage.local.set({ focusMode });
-    });
-  };
-
-  const setupSiteToggle = () => {
-    siteToggle.textContent = blockedSites.includes(baseUrl) ? "Remove From Blocklist" : "Add To Blocklist";
-    siteToggle.addEventListener("click", () => {
-      if (blockedSites.includes(baseUrl)) {
-        blockedSites = blockedSites.filter((element) => element !== baseUrl);
-        chrome.tabs.sendMessage(tabs[0].id, "Removed From Block List");
-        siteToggle.textContent = "Add To Block List";
-      } else {
-        blockedSites.push(baseUrl);
-        chrome.tabs.sendMessage(tabs[0].id, "Added To Block List");
-        siteToggle.textContent = "Remove From Block List";
-      }
-      chrome.storage.local.set({ blockedSites });
-    });
-  };
-
-  const setupSiteTodayUsage = () => {
-    const siteHrs = Math.floor((storage.tabsTime[domain] ?? 0) / 3600000);
-    const siteMins = Math.floor(((storage.tabsTime[domain] ?? 0) % 3600000) / 60000);
-    siteTodayUsage.textContent = `This Site: ${siteHrs} ${siteHrs === 0 ? "hr" : "hrs"} ${siteMins} ${
-      siteMins === 0 ? "min" : "mins"
-    }`;
-  };
-
-  const setupSitesTodayUsage = () => {
-    const sitesTodaySeconds = Object.values(storage.tabsTime).reduce((acc, curr) => acc + curr, 0);
-    const sitesHrs = Math.floor(sitesTodaySeconds / 3600000);
-    const sitesMins = Math.floor((sitesTodaySeconds % 3600000) / 60000);
-    sitesTodayUsage.textContent = `All Sites: ${sitesHrs} ${sitesHrs === 0 ? "hr" : "hrs"} ${sitesMins} ${
-      sitesMins === 0 ? "min" : "mins"
-    }`;
-  };
-
-  setupFocusToggle();
-  setupSiteToggle();
-  setupSiteTodayUsage();
-  setupSitesTodayUsage();
+  focusBlock.textContent = "Focus Tab";
 });
 
 // For debugging
