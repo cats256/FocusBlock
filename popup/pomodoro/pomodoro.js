e340ccb0cca5d535a2b3ea47cfb61a4148923cf1
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-22 22:35:23 -0500

    Refactor code

--- popup/pomodoro/pomodoro.js
+++ popup/pomodoro/pomodoro.js
@@ -2,82 +2,79 @@ const pomodoroHTML = await fetch("pomodoro/pomodoro.html");
 const pomodoroHTMLText = await pomodoroHTML.text();
 document.getElementById("pomodoro-timer").innerHTML = pomodoroHTMLText;
 
-const focus_minute_input = document.getElementById("focus-minutes");
-const break_minute_input = document.getElementById("break-minutes");
-const sessions_input = document.getElementById("sessions");
+const focusMinuteInput = document.getElementById("focus-minutes");
+const breakMinuteInput = document.getElementById("break-minutes");
+const sessionsInput = document.getElementById("sessions");
 
-const start_button = document.getElementById("start");
-const reset_button = document.getElementById("reset");
-const back_button = document.getElementById("back");
+const startButton = document.getElementById("start");
+const resetButton = document.getElementById("reset");
 
-let pomodoro_information = {};
-let current_session = 0;
-let pomodoro_timer = null;
+let pomodoroInformation = {};
+let currentSession = 0;
+let pomodoroTimer = null;
 
 const reset = () => {
   chrome.storage.local.set({ pomodoro_info: {}, pomodoro: false });
-  clearInterval(pomodoro_timer);
-  current_session = 0;
+  clearInterval(pomodoroTimer);
+  currentSession = 0;
   document.getElementById("clock").textContent = "00:00";
-  document.getElementById("type").textContent = "";
 };
 
-const check_finished = () => {
-  console.log(`current: ${current_session} total: ${pomodoro_information.cycles}`);
-  if (current_session == pomodoro_information.cycles) {
-    clearInterval(pomodoro_timer);
+const checkFinished = () => {
+  console.log(`current: ${currentSession} total: ${pomodoroInformation.cycles}`);
+  if (currentSession === pomodoroInformation.cycles) {
+    clearInterval(pomodoroTimer);
     console.log("Cleared");
     document.getElementById("clock").textContent = "00:00";
     reset();
   }
 
-  if (pomodoro_information.focus) {
-    document.getElementById("type").textContent = `Focus ${current_session}`;
+  if (pomodoroInformation.focus) {
+    document.getElementById("type").textContent = `Focus ${currentSession}`;
   } else {
-    document.getElementById("type").textContent = `Break ${current_session}`;
+    document.getElementById("type").textContent = `Break ${currentSession}`;
   }
 };
 
-const generate_pomodoro_information = async () => {
+const generatePomodoroInformation = async () => {
   const storage = await chrome.storage.local.get();
   if (storage.pomodoro) {
-    pomodoro_information = storage.pomodoro_info;
-    console.log(pomodoro_information);
+    pomodoroInformation = storage.pomodoro_info;
+    console.log(pomodoroInformation);
 
-    focus_minute_input.value = parseInt(pomodoro_information.focus_time);
-    break_minute_input.value = parseInt(pomodoro_information.break_time);
+    focusMinuteInput.value = parseInt(pomodoroInformation.focus_time, 10);
+    breakMinuteInput.value = parseInt(pomodoroInformation.break_time, 10);
     // console.log(pomodoro_information.cycles);
-    sessions_input.value = parseInt(pomodoro_information.cycles);
-    let check_date = Date.now();
+    sessionsInput.value = parseInt(pomodoroInformation.cycles, 10);
+    const checkDate = Date.now();
 
-    if (check_date > pomodoro_information.all_session_info.slice(-1)[0].end_time) {
+    if (checkDate > pomodoroInformation.all_session_info.slice(-1)[0].end_time) {
       chrome.storage.local.set({ pomodoro: false });
     }
 
-    for (let session_info of pomodoro_information.all_session_info) {
-      if (check_date < session_info.end_time) {
-        pomodoro_information.end_time = session_info.end_time;
-        pomodoro_information.start_time = check_date;
-        current_session = session_info.session;
+    pomodoroInformation.all_session_info.forEach((sessionInfo) => {
+      if (checkDate < sessionInfo.end_time) {
+        pomodoroInformation.end_time = sessionInfo.end_time;
+        pomodoroInformation.start_time = checkDate;
+        currentSession = sessionInfo.session;
 
-        if (session_info.type == "focus") {
-          pomodoro_information.focus = true;
-          pomodoro_information.break = false;
+        if (sessionInfo.type === "focus") {
+          pomodoroInformation.focus = true;
+          pomodoroInformation.break = false;
         } else {
-          pomodoro_information.focus = false;
-          pomodoro_information.break = true;
+          pomodoroInformation.focus = false;
+          pomodoroInformation.break = true;
         }
-        break;
       }
-    }
+    });
   } else {
-    current_session = 0;
-    pomodoro_information = {
+    currentSession = 0;
+    pomodoroInformation = {
       focus: true,
       break: false,
-      focus_time: focus_minute_input.value,
-      break_time: break_minute_input.value,
-      cycles: sessions_input.value,
+      focus_time: focusMinuteInput.value,
+      break_time: breakMinuteInput.value,
+      cycles: sessionsInput.value,
       end_time: null,
       start_time: null,
       time_info: {},
@@ -88,35 +85,35 @@ const generate_pomodoro_information = async () => {
     chrome.storage.local.set({ pomodoro: true }); // setting the start of the pomodoro timer
 
     // initiate the time info
-    let start_time = Date.now();
-    let temp = start_time;
-    for (let i = 0; i < sessions_input.value; i++) {
-      let temp_array = [];
-      temp = temp + focus_minute_input.value * 60 * 1000;
-      let focus_data = { type: "focus", end_time: temp, session: i };
-      temp = temp + break_minute_input.value * 60 * 1000;
-      let break_data = { type: "break", end_time: temp, session: i };
+    const startTime = Date.now();
+    let temp = startTime;
+    for (let i = 0; i < sessionsInput.value; i += 1) {
+      const tempArray = [];
+      temp += focusMinuteInput.value * 60 * 1000;
+      const focusData = { type: "focus", end_time: temp, session: i };
+      temp += breakMinuteInput.value * 60 * 1000;
+      const breakData = { type: "break", end_time: temp, session: i };
 
-      temp_array.push(focus_data);
-      temp_array.push(break_data);
-      pomodoro_information.all_session_info.push(focus_data);
-      pomodoro_information.all_session_info.push(break_data);
+      tempArray.push(focusData);
+      tempArray.push(breakData);
+      pomodoroInformation.all_session_info.push(focusData);
+      pomodoroInformation.all_session_info.push(breakData);
 
-      pomodoro_information.session_info.push(temp_array);
+      pomodoroInformation.session_info.push(tempArray);
     }
 
-    pomodoro_information.start_time = start_time;
-    pomodoro_information.end_time = pomodoro_information.session_info[0][0].end_time;
-    chrome.storage.local.set({ pomodoro_info: pomodoro_information });
-    console.log(pomodoro_information);
+    pomodoroInformation.start_time = startTime;
+    pomodoroInformation.end_time = pomodoroInformation.session_info[0][0].end_time;
+    chrome.storage.local.set({ pomodoro_info: pomodoroInformation });
+    console.log(pomodoroInformation);
   }
 };
 
-const get_time_difference = (end_time) => {
-  const start_time = Date.now();
-  const time_diff = end_time - start_time;
+const getTimeDifference = (endTime) => {
+  const startTime = Date.now();
+  const timeDiff = endTime - startTime;
 
-  const total = Number.parseInt(time_diff / 1000, 10);
+  const total = Number.parseInt(timeDiff / 1000, 10);
   const minutes = Number.parseInt((total / 60) % 60, 10);
   const seconds = Number.parseInt(total % 60, 10);
 
@@ -127,39 +124,40 @@ const get_time_difference = (end_time) => {
   };
 };
 
-const change_time = () => {
-  let { total, minutes, seconds } = get_time_difference(pomodoro_information.end_time);
+const changeTime = () => {
+  const { total } = getTimeDifference(pomodoroInformation.end_time);
+  let { minutes, seconds } = getTimeDifference(pomodoroInformation.end_time);
   minutes = `${minutes}`.padStart(2, "0");
   seconds = `${seconds}`.padStart(2, "0");
-  pomodoro_information.time_info = { total, minutes, seconds };
+  pomodoroInformation.time_info = { total, minutes, seconds };
 
   if (total <= 0) {
-    console.log(`current session: ${current_session}`);
-    if (pomodoro_information.focus) {
+    console.log(`current session: ${currentSession}`);
+    if (pomodoroInformation.focus) {
       // pomodoro_information.start_time = Date.now();
-      pomodoro_information.focus = false;
-      pomodoro_information.break = true;
-      pomodoro_information.end_time = pomodoro_information.session_info[current_session][1].end_time;
-      check_finished();
-    } else if (pomodoro_information.break) {
+      pomodoroInformation.focus = false;
+      pomodoroInformation.break = true;
+      pomodoroInformation.end_time = pomodoroInformation.session_info[currentSession][1].end_time;
+      checkFinished();
+    } else if (pomodoroInformation.break) {
       // pomodoro_information.start_time = Date.now();
-      pomodoro_information.focus = true;
-      pomodoro_information.break = false;
-      current_session = current_session + 1;
-      check_finished();
-      pomodoro_information.end_time = pomodoro_information.session_info[current_session][0].end_time;
+      pomodoroInformation.focus = true;
+      pomodoroInformation.break = false;
+      currentSession += 1;
+      checkFinished();
+      pomodoroInformation.end_time = pomodoroInformation.session_info[currentSession][0].end_time;
     }
   }
 
   // check_finished();
 
-  chrome.storage.local.set({ pomodoro_info: pomodoro_information });
+  chrome.storage.local.set({ pomodoro_info: pomodoroInformation });
   document.getElementById("clock").textContent = `${minutes}:${seconds}`;
 };
 
-start_button.addEventListener("click", () => {
-  generate_pomodoro_information();
-  pomodoro_timer = setInterval(change_time, 1000);
+startButton.addEventListener("click", () => {
+  generatePomodoroInformation();
+  pomodoroTimer = setInterval(changeTime, 1000);
   // chrome.runtime.sendMessage({ type: "start pomodoro" });
 });
 
@@ -167,15 +165,15 @@ start_button.addEventListener("click", () => {
 //   document.location.href = "../popup.html";
 // });
 
-reset_button.addEventListener("click", () => {
+resetButton.addEventListener("click", () => {
   reset();
 });
 
 const resume = async () => {
   const storage = await chrome.storage.local.get();
   if (storage.pomodoro) {
-    generate_pomodoro_information();
-    pomodoro_timer = setInterval(change_time, 1000);
+    generatePomodoroInformation();
+    pomodoroTimer = setInterval(changeTime, 1000);
   }
 };
 
