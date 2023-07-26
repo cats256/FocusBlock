6925074f4411bff1b9e12bed764410c1194fbbdc
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-25 21:40:20 -0500

    Make minor changes

--- popup/popup.js
+++ popup/popup.js
@@ -8,52 +8,42 @@ const timer = document.getElementById("timer");
 const powerSettings = document.getElementById("power-settings");
 const settingsButton = document.getElementById("settings");
 
-const setupPomodoroTimerBtn = () => {
-  timer.addEventListener("click", () => {
-    pomodoroTimer.style.display = "block";
-    focusTab.style.display = "none";
-    settingsTab.style.display = "none";
+timer.addEventListener("click", () => {
+  pomodoroTimer.style.display = "block";
+  focusTab.style.display = "none";
+  settingsTab.style.display = "none";
 
-    timer.style.backgroundColor = "rgb(205, 205, 205)";
-    settingsButton.style.backgroundColor = "";
-    powerSettings.style.backgroundColor = "";
+  timer.style.backgroundColor = "rgb(205, 205, 205)";
+  settingsButton.style.backgroundColor = "";
+  powerSettings.style.backgroundColor = "";
 
-    document.getElementById("focus-block").textContent = "Pomodoro Timer";
-  });
-};
+  document.getElementById("focus-block").textContent = "Pomodoro Timer";
+});
 
-const setupPowerSettingsBtn = () => {
+powerSettings.style.backgroundColor = "rgb(205, 205, 205)";
+powerSettings.addEventListener("click", () => {
+  pomodoroTimer.style.display = "none";
+  focusTab.style.display = "flex";
+  settingsTab.style.display = "none";
+
+  timer.style.backgroundColor = "";
+  settingsButton.style.backgroundColor = "";
   powerSettings.style.backgroundColor = "rgb(205, 205, 205)";
-  powerSettings.addEventListener("click", () => {
-    pomodoroTimer.style.display = "none";
-    focusTab.style.display = "flex";
-    settingsTab.style.display = "none";
 
-    timer.style.backgroundColor = "";
-    settingsButton.style.backgroundColor = "";
-    powerSettings.style.backgroundColor = "rgb(205, 205, 205)";
+  focusBlock.textContent = "Focus Tab";
+});
 
-    focusBlock.textContent = "Focus Tab";
-  });
-};
+settingsButton.addEventListener("click", () => {
+  pomodoroTimer.style.display = "none";
+  focusTab.style.display = "none";
+  settingsTab.style.display = "block";
 
-const setupSettingsButton = () => {
-  settingsButton.addEventListener("click", () => {
-    pomodoroTimer.style.display = "none";
-    focusTab.style.display = "none";
-    settingsTab.style.display = "block";
+  timer.style.backgroundColor = "";
+  settingsButton.style.backgroundColor = "rgb(205, 205, 205)";
+  powerSettings.style.backgroundColor = "";
 
-    timer.style.backgroundColor = "";
-    settingsButton.style.backgroundColor = "rgb(205, 205, 205)";
-    powerSettings.style.backgroundColor = "";
-
-    focusBlock.textContent = "Settings";
-  });
-};
-
-setupPomodoroTimerBtn();
-setupPowerSettingsBtn();
-setupSettingsButton();
+  focusBlock.textContent = "Settings";
+});
 
 // For debugging
 chrome.storage.local.get().then((storage) => {
