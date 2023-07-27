692fa3db1d79768d4f95cc8e668c9472d10c2bc1
cats256 <59489624+cats256@users.noreply.github.com>
2023-07-27 16:30:17 -0500

    Add preview for icons

--- options/options.js
+++ options/options.js
@@ -1,6 +1,21 @@
 const backgroundImageDropdown = document.getElementById("background-img-dropdown");
+const backgroundImagePreview = document.getElementById("background-img-preview");
+const { backgroundImage } = await chrome.storage.local.get(["backgroundImage"]);
 
-backgroundImageDropdown.addEventListener("change", async (e) => {
-  const { value } = e.target;
-  chrome.storage.local.set({ backgroundImage: value });
+switch (backgroundImage) {
+  case "icons/lotus.svg":
+    backgroundImageDropdown.selectedIndex = 1;
+    break;
+  case "icons/icon.png":
+    backgroundImageDropdown.selectedIndex = 2;
+    break;
+  default:
+    backgroundImageDropdown.selectedIndex = 0;
+    break;
+}
+backgroundImagePreview.src = `../${backgroundImage}`;
+
+backgroundImageDropdown.addEventListener("change", (e) => {
+  backgroundImagePreview.src = `../${e.target.value}`;
+  chrome.storage.local.set({ backgroundImage: e.target.value });
 });
